b2e527ed551ea8cf81f329ffd2f1f3e14ca14172
Alex <alex5402private@gmail.com>
2024-07-23 20:49:36 +0530

    update : Tue Jul 23 08:49:36 PM IST 2024

--- README.md
+++ README.md
@@ -6,16 +6,11 @@
   ![demo](https://github.com/ALEX5402/dotfiles/assets/76860596/ff68bba7-e8da-49d3-a716-3ed3d73cfc25)
 
  
-## Changelog Tue Jul 23 08:38:35 PM IST 2024
+## Changelog Tue Jul 23 08:49:36 PM IST 2024
 ```
- Configs/hyde/hyde.conf                        |   2 +-
- Configs/hyde/themes/Catppuccin Mocha/wall.set |   2 +-
- Configs/hypr/themes/theme.conf                |  41 ++++++---
- Configs/kitty/kitty.conf                      |   1 +
- Configs/kitty/theme.conf                      | 128 +++++++++++++++-----------
- Configs/waybar/config.jsonc                   |   2 +-
- Configs/waybar/theme.css                      |  12 +--
- README.md                                     |  13 +--
- 8 files changed, 113 insertions(+), 88 deletions(-)
+ README.md             | 13 +------------
+ dependencies-list.txt | 12 +++++++-----
+ install-packages.sh   | 31 +++++++++++++++++++++++--------
+ 3 files changed, 31 insertions(+), 25 deletions(-)
 ```
  
--- install-packages.sh
+++ install-packages.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
 
-# Read the dependencies file
-file="$1"
+# Check if a package is installed
+is_installed() {
+    pacman -Qi "$1" &> /dev/null
+    return $?
+}
 
 # Function to install packages using paru
 install_packages() {
+    local file="$1"
+
     while IFS= read -r line || [[ -n "$line" ]]; do
         # Skip empty lines and lines starting with #
         [[ -z "$line" || $line == \#* ]] && continue
@@ -12,16 +17,26 @@ install_packages() {
         # Extract the package name by removing comments
         package=$(echo "$line" | cut -d '#' -f 1 | xargs)
         
-        # If package is non-empty, install it using paru
+        # If package is non-empty, check if it's installed
         if [[ -n "$package" ]]; then
-            echo "Installing $package..."
-            paru -S --noconfirm "$package"
+            if is_installed "$package"; then
+                echo "$package is already installed. Skipping..."
+            else
+                echo "Installing $package..."
+                paru -S --noconfirm "$package"
+            fi
         fi
     done < "$file"
 }
 
-# Run the install_packages function
-install_packages
+# Check if the script received a file argument
+if [[ -z "$1" ]]; then
+    echo "Usage: $0 <path-to-txt-file>"
+    exit 1
+fi
 
-echo "All packages installed."
+# Run the install_packages function with the provided file
+install_packages "$1"
+
+echo "All packages processed."
 
