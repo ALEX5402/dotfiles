89083b7fd166942df528654c0a6cc5d1d6e47246
Alex <alex5402private@gmail.com>
2024-08-14 11:07:54 +0530

    update : Wed Aug 14 11:07:54 AM IST 2024

---
+++ Local/share/bin/apply-cursor.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env sh
+gsettings set org.gnome.desktop.interface cursor-theme 'Bibata-Modern-Classic'
+gsettings set org.gnome.desktop.interface cursor-size 24
+hyprctl setcursor Bibata-Modern-Classic 24
