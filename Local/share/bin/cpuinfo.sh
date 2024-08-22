e12702b9dd6ad14711ca0a6e1ff530df3d9f5dac
Alex <alex5402private@gmail.com>
2024-08-22 20:02:08 +0530

    update : Thu Aug 22 08:02:07 PM IST 2024

--- Local/share/bin/cpuinfo.sh
+++ Local/share/bin/cpuinfo.sh
@@ -1,35 +1,59 @@
 #!/usr/bin/env sh
 
 # CPU model
-model=$(cat /proc/cpuinfo | grep 'model name' | head -n 1 | awk -F ': ' '{print $2}')
+model=$(grep -m 1 'model name' /proc/cpuinfo | awk -F ': ' '{print $2}')
 
 # CPU utilization
 utilization=$(top -bn1 | awk '/^%Cpu/ {print 100 - $8}')
 
 # Clock speed
-freqlist=$(cat /proc/cpuinfo | grep "cpu MHz" | awk '{ print $4 }')
-maxfreq=$(cat /sys/devices/system/cpu/cpu0/cpufreq/cpuinfo_max_freq | sed 's/...$//')
-frequency=$(echo $freqlist | tr ' ' '\n' | awk "{ sum+=\$1 } END {printf \"%.0f/$maxfreq MHz\", sum/NR}")
+freqlist=$(grep "cpu MHz" /proc/cpuinfo | awk '{ print $4 }')
+maxfreq=$(($(cat /sys/devices/system/cpu/cpu0/cpufreq/cpuinfo_max_freq) / 1000))
+frequency=$(echo "$freqlist" | awk '{ sum+=$1 } END {printf "%.0f/%d MHz", sum/NR, maxfreq}')
 
-# CPU temp
+# CPU temperature
 temp=$(sensors | awk '/Package id 0/ {print $4}' | awk -F '[+.]' '{print $2}')
 if [ -z "$temp" ]; then
     temp=$(sensors | awk '/Tctl/ {print $2}' | tr -d '+°C')
 fi
-if [ -z "$temp" ]; then
-    temp="N/A"
-fi
+[ -z "$temp" ] && temp="N/A"
 
-# map icons
-set_ico="{\"thermo\":{\"0\":\"\",\"45\":\"\",\"65\":\"\",\"85\":\"\"},\"emoji\":{\"0\":\"❄️\",\"45\":\"☁️\",\"65\":\"🔥\",\"85\":\"🌋\"},\"util\":{\"0\":\"󰾆\",\"30\":\"󰾅\",\"60\":\"󰓅\",\"90\":\"\"}}"
+# Convert temperature to integer (strip decimal part)
+temp_int=$(echo "$temp" | awk -F '.' '{print $1}')
+
+# Icon mapping function
 eval_ico() {
-    map_ico=$(echo "${set_ico}" | jq -r --arg aky "$1" --argjson avl "$2" '.[$aky] | keys_unsorted | map(tonumber) | map(select(. <= $avl)) | max')
-    echo "${set_ico}" | jq -r --arg aky "$1" --arg avl "$map_ico" '.[$aky] | .[$avl]'
+    local aky="$1"
+    local avl="$2"
+    local map_ico=$(jq -r --arg aky "$aky" --argjson avl "$avl" '.[$aky] | keys_unsorted | map(tonumber) | map(select(. <= $avl)) | max' <<< "$set_ico")
+    jq -r --arg aky "$aky" --arg avl "$map_ico" '.[$aky] | .[$avl]' <<< "$set_ico"
 }
 
-thermo=$(eval_ico thermo $temp)
-emoji=$(eval_ico emoji $temp)
-speedo=$(eval_ico util $utilization)
+# Icon map JSON
+set_ico='{
+    "thermo":{"0":"","45":"","65":"","85":""},
+    "emoji":{"0":"❄️","45":"☁️","65":"🔥","85":"🌋"},
+    "util":{"0":"󰾆","30":"󰾅","60":"󰓅","90":""}
+}'
 
-# Print cpu info (json)
+# Evaluate icons based on temperature and utilization
+thermo=$(eval_ico thermo "$temp_int")
+emoji=$(eval_ico emoji "$temp_int")
+speedo=$(eval_ico util "$utilization")
+
+# Notification function using hyprctl
+send_notification() {
+    local temp="$1"
+    local emoji="$2"
+#     hyprctl notify 2 1500 "rgb(E5BD49)" "fontsize:20 ${emoji} High CPU Temperature: ${temp}°C"
+      notify-send -t 1500 -i ~/.config/dunst/icons/cpu-fire.svg "${emoji} High CPU Temperature" "Current temperature: ${temp}°C"
+
+}
+
+# Send notification if temperature exceeds 70°C
+if [ "$temp_int" != "N/A" ] && [ "$temp_int" -gt 75 ]; then
+    send_notification "$temp" "$emoji"
+fi
+
+# Output JSON
 echo "{\"text\":\"${thermo} ${temp}°C\", \"tooltip\":\"${model}\n${thermo} Temperature: ${temp}°C ${emoji}\n${speedo} Utilization: ${utilization}%\n Clock Speed: ${frequency}\"}"
--- README.md
+++ README.md
@@ -22,11 +22,53 @@
 
 # Then simply links the folders by filemager
  
-## Changelog Thu Aug 22 01:10:23 PM IST 2024
+## Changelog Thu Aug 22 08:02:07 PM IST 2024
 ```
- Configs/hypr/hypridle.conf | 14 +++++++-------
- README.md                  |  7 +------
- packages-with-info.txt     |  2 --
- 3 files changed, 8 insertions(+), 15 deletions(-)
+ Configs/Kvantum/wallbash/wallbash.kvconfig  |   98 +--
+ Configs/Kvantum/wallbash/wallbash.svg       | 1196 +++++++++++++--------------
+ Configs/ags/user/colors-ags.css             |   38 +-
+ Configs/cava/Wall-Dcol                      |   16 +-
+ Configs/cava/config                         |   16 +-
+ Configs/dunst/dunstrc                       |   12 +-
+ Configs/dunst/icons/hyprdots.svg            |   12 +-
+ Configs/dunst/icons/vol/muted-mic.svg       |    2 +-
+ Configs/dunst/icons/vol/muted-speaker.svg   |    2 +-
+ Configs/dunst/icons/vol/unmuted-mic.svg     |    2 +-
+ Configs/dunst/icons/vol/unmuted-speaker.svg |    2 +-
+ Configs/dunst/icons/vol/vol-0.svg           |   10 +-
+ Configs/dunst/icons/vol/vol-10.svg          |   10 +-
+ Configs/dunst/icons/vol/vol-100.svg         |   10 +-
+ Configs/dunst/icons/vol/vol-15.svg          |   10 +-
+ Configs/dunst/icons/vol/vol-20.svg          |   10 +-
+ Configs/dunst/icons/vol/vol-25.svg          |   10 +-
+ Configs/dunst/icons/vol/vol-30.svg          |   10 +-
+ Configs/dunst/icons/vol/vol-35.svg          |   10 +-
+ Configs/dunst/icons/vol/vol-40.svg          |   10 +-
+ Configs/dunst/icons/vol/vol-45.svg          |   10 +-
+ Configs/dunst/icons/vol/vol-5.svg           |   10 +-
+ Configs/dunst/icons/vol/vol-50.svg          |   10 +-
+ Configs/dunst/icons/vol/vol-55.svg          |   10 +-
+ Configs/dunst/icons/vol/vol-60.svg          |   10 +-
+ Configs/dunst/icons/vol/vol-65.svg          |   10 +-
+ Configs/dunst/icons/vol/vol-70.svg          |   10 +-
+ Configs/dunst/icons/vol/vol-75.svg          |   10 +-
+ Configs/dunst/icons/vol/vol-80.svg          |   10 +-
+ Configs/dunst/icons/vol/vol-85.svg          |   10 +-
+ Configs/dunst/icons/vol/vol-90.svg          |   10 +-
+ Configs/dunst/icons/vol/vol-95.svg          |   10 +-
+ Configs/dunst/wallbash.conf                 |   12 +-
+ Configs/hyde/themes/Sweet-Dark/wall.set     |    2 +-
+ Configs/hypr/themes/colors.conf             |   10 +-
+ Configs/kitty/theme.conf                    |   48 +-
+ Configs/qt5ct/colors.conf                   |    6 +-
+ Configs/qt6ct/colors.conf                   |    6 +-
+ Configs/rofi/theme.rasi                     |   12 +-
+ Configs/rofi/wallust/colors-rofi.rasi       |    6 +-
+ Configs/spicetify/Themes/Sleek/color.ini    |   40 +-
+ Configs/swappy/config                       |    2 +-
+ Configs/waybar/theme.css                    |   14 +-
+ Local/share/bin/cpuinfo.sh                  |   56 +-
+ README.md                                   |    8 +-
+ 45 files changed, 923 insertions(+), 905 deletions(-)
 ```
  
